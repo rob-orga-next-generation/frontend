64c62a501a6244b82e37d28000961a7a1ea8930a
losrobbos <losrobbos@web.de>
2022-05-13 13:48:00 +0200

    Moved files to types folder

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../styles/App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  )
+}
+
+export default App
